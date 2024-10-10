84c17febca230e98d438ce25efab8e1a9daf0ba8
kaylaosullivan <k.osullivan.steben@hotmail.com>
2024-10-10 18:14:57 +0000

    Keep list of patients not deidentified and with errors

--- check_anon.py
+++ check_anon.py
@@ -9,22 +9,28 @@ PATH = '/mnt/iDriveShare/Kayla/CBCT_images/kayla_extracted/'
 one_per_dir = True
 one_per_unsorted = True
 unsorted_checked = False
+patients_not_deidentified = []
+patients_with_errors = []
 
-
-def identity_removed(file_path,file_name):
+def identity_removed(file_path,file_name,patient):
     try:
 
         if dcm.read_file(os.path.join(file_path,file_name)).PatientIdentityRemoved != 'YES':
             
-            print("ATTENTION: Found DICOMs with identifiers in " + file_path)
+            print("ATTENTION: Found DICOMs with identifiers in Patient "+patient+" directory : " + file_path)
+            if patient not in patients_not_deidentified:
+                patients_not_deidentified.append(patient)
             return False
         else:
             return True
 
     except Exception as e:
         d_file = dcm.read_file(os.path.join(file_path,file_name))
+        print(file_path,file_name)
         if d_file.PatientSex != ''  or d_file.PatientBirthDate != '' or '^' in d_file.PatientName or ' ' in d_file.PatientName or ',' in d_file.patientName:
-            print("ATTENTION: Found DICOMs with identifiers in Patient "+patient+" directory "+file_path)
+            print("ATTENTION: Found DICOMs with identifiers in Patient "+patient+" directory :"+file_path)
+            if patient not in patients_not_deidentified:
+                patients_not_deidentified.append(patient)
             return False
 
             
@@ -32,6 +38,8 @@ def identity_removed(file_path,file_name):
            # if e =="'FileDataset' object has no attribute 'PatientIdentityRemoved'":
 
             print("Error with file " + file_path+f,":",e)
+            if patient not in patients_with_errors:
+                patients_with_errors.append(patient)
             return True
 
     #print( dcm.read_file(os.path.join(PATH,patient,file_path,file_name)).PatientIdentityRemoved )
@@ -47,19 +55,20 @@ for patient in sorted(os.listdir(PATH)):
     for d in os.listdir(patient_path):
         if d.endswith(".dcm") and not (one_per_unsorted and unsorted_checked):
             
-            is_deidentified *= identity_removed(patient_path,d)
+            is_deidentified *= identity_removed(patient_path,d,patient)
             unsorted_checked = True 
 
 
         if os.path.isdir(os.path.join(patient_path,d)):
             #print(d)
             file_path = os.path.join(patient_path,d) # Assuming only one level of directories
-            for f in os.listdir(file_path):
+            for f in sorted(os.listdir(file_path)): # will choose CT before RS, sometimes tags missing in RS
                 if os.path.isfile(os.path.join(file_path,f)):
                     #print("**************************************************************************************************************************************************") 
                     #print(dcm.read_file(os.path.join(file_path,f)))
-                    
-                    is_deidentified *= identity_removed(file_path,f)
+                    #if f[0:2] == "RS":
+                     #   print("doing RS")
+                    is_deidentified *= identity_removed(file_path,f,patient)
                     
                     if one_per_dir:
                         break
@@ -71,9 +80,13 @@ for patient in sorted(os.listdir(PATH)):
     if is_deidentified:
         print("OK")
     else:
-        print("CAUTION - "+patient+"  NOT DEINTIFIED")
+        print("CAUTION - "+patient+"  NOT DEIDENTIFIED")
 
-        
+print("**************************************************")
+print(len(patients_not_deidentified),"patients not deidentified:",patients_not_deidentified)
+print(len(patients_with_errors),"patients with errors:",patients_with_errors)
 print("--- %s seconds ---" % (time.time() - start_time))
+print("**************************************************")
+
 #if dcm.read_file(PATH).PatientIdentityRemoved != 'YES':
   #  print("ATTENTION: ")
