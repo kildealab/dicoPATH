e4aafe79b1e0289221033c5abe977fb6ddb3bdb4
Kayla O'Sullivan <k.osullivan.steben@hotmail.com>
2025-01-08 21:07:57 +0000

    Format config for general use:

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 ''' 
 Configuration variables for runing the code. See below for more information. 
-PATH is the only variable you NEED to change, but it is recommended to change the CT keyword too.
+PATH is the only variable you NEED to change, but it is recommended to change the CT keyword and ignore terms too.
 '''
 
 config = {
-	'PATH':'/mnt/iDriveShare/Kayla/CBCT_images/anonymized/phant/',#'/mnt/iDriveShare/Kayla/CBCT_images/REMOVED/', # list to patient directories
-	'CT_keyword':'CT_', # A keyword present in all CT names from the TPS, helps to differentiate between CBCT names
-	'CT_name_min_length':9, # minimum string length of CT name
+	'PATH':'/path/to/patient/directories/', # list to patient directories
+	'CT_keyword':'', # A keyword present in all CT names from the TPS, helps to differentiate between CBCT names
+	'CT_name_min_length':0, # minimum string length of CT name
 	'CT_name_max_length':16, # maximum string length allowed in Structure Set Label tag = 16
-	'ignore_keywords_in_CT':['copy', 'adap', 'planad', 'qa', 'test','pa','do not use'], # Words to ignore in CT names (ie not real planning CTs)
+	'ignore_keywords_in_CT':['copy', 'qa', 'test','do not use'], # Words to ignore in CT names (ie not real planning CTs)
 	'ignore_keywords_in_pt_dirname':[], # Keywords in patient directories to skip, eg if you want to skip all dirs with 'old' in them
-	'print_check_results': True
+	'print_check_results': True # Prints results to console for additional scripts 
 }
 
 '''
@@ -22,7 +22,8 @@ customize the following variables to work with your naming convention.
  - CT_keyword: a keyword present in all CT names form the TPS
  - CT_name_min_length: the minimum length of the CT name
  - CT_name_max_length: the maximum length of the CT name
- - ignore_keywords_in_CT:
+ - ignore_keywords_in_CT: keywords frequently found in CT copies created in the TPS that are not useful. Often contain words
+ 						  like 'copy', 'QA', 'test', etc.
 For example, our CTs are always named in the format "CT_16_JUN_2024", so I might set CT_keyword = 'CT_' and the min and
 max lengths to 14. Doing so would ignore names like "CT_1", which in our case is simply an unwanted copy.
 
